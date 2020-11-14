c74b0253dbdb838e94685e439c9888bb38052375
Christian O'Neill <gusonech@student.gu.se>
2020-11-14 11:37:57 +0100

    Added MQTT dependency
    Added basic publish functionality.

---
+++ server/mqtt/publisher.js
@@ -0,0 +1,17 @@
+const mqtt = require('mqtt');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = mqtt.connect({
+  host: process.env.MQTT_HOST,
+  port: process.env.MQTT_PORT
+});
+
+const topic = 'dentist-test';
+const message = 'You have subscribed to Dentist Daily!';
+
+client.on('connect', () => {
+  console.log(' >> Publisher connected...');
+  client.publish(topic, message);
+});
