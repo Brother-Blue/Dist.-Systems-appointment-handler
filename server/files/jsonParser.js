fc6dc718702e7e54408d8340047b596d9c5ecd54
jensenb <jensenb@student.chalmers.se>
2020-11-30 16:51:58 +0100

    Preliminary attempt at adding data to database

--- server/files/jsonParser.js
+++ server/files/jsonParser.js
@@ -1,4 +1,13 @@
 const fetch = require('node-fetch');
+const mongodb = require('mongodb');
+const mongoClient = mongodb.MongoClient;
+
+var db
+
+mongoClient.connect("mongodb+srv://123123123:123123123@cluster0.5paxo.mongodb.net/Cluster0?retryWrites=true&w=majority", { useUnifiedTopology: true }, (err, client) => {
+  if (err) return console.error(err);
+  db = client.db('root-test');
+});
 
 //Code snippet found here: https://dev.to/isalevine/three-ways-to-retrieve-json-from-the-web-using-node-js-3c88
 
@@ -8,5 +17,23 @@ let settings = { method: "Get" };
 fetch(url, settings)
     .then(res => res.json())
     .then((json) => {
-        console.log(json);
+        db.collection('dentistoffices').insertMany({
+            id: json.id,
+            name: json.name,
+            owner: json.owner,
+            dentists: json.dentists,
+            address: json.address,
+            city: json.city,
+            coordinate: [{
+            latitude: json.coordinate.latitude,
+            longitude: json.coordinate.longitude,
+        }],
+        openinghours: [{
+            monday: json.openinghours.monday,
+            tuesday: json.openinghours.tuesday,
+            wednesday: json.openinghours.wednesday,
+            thursday: json.openinghours.thursday,
+            friday: json.openinghours.friday,
+        }]
+    })
 });
