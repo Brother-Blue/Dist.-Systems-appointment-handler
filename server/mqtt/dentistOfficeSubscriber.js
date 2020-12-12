0c6178fc5208a568d33b3bd8f0e661d19f4b8c0a
ilinus <ilinus@student.chalmers.se>
2020-12-12 13:48:53 +0100

    Made use of the Opossum-library to use a circuitbreaker

--- server/mqtt/dentistOfficeSubscriber.js
+++ server/mqtt/dentistOfficeSubscriber.js
@@ -5,11 +5,17 @@ const mongoClient = mongodb.MongoClient;
 const fetch = require('node-fetch');
 const { publish } = require('./publisher')
 const deviceRoot = 'dentistimo/';
+const CircuitBreaker = require('opossum')
 
 dotenv.config();
 
+const options = {
+    timeout: 3000, //If function takes longer than 3sec, trigger a failure
+    errorHandlingPercentage: 50, //If 50% of requests fail, trigger circuit
+    resetTimeout: 3000 //After xx seconds try again
+}
 let db;
-let url = "https://raw.githubusercontent.com/feldob/dit355_2020/master/dentists.json";
+let url = "https://ra.githubusercontent.com/feldob/dit355_2020/master/dentists.json";
 let settings = { method: "Get" };
 
 const client = mqtt.connect({
@@ -20,8 +26,13 @@ const client = mqtt.connect({
 mongoClient.connect("mongodb+srv://123123123:123123123@cluster0.5paxo.mongodb.net/Cluster0?retryWrites=true&w=majority", { useUnifiedTopology: true }, (err, client) => {
   if (err) return console.error(err);
   db = client.db('root-test');
-  updateDentistOffices();
-});
+  const breaker = new CircuitBreaker(updateDentistOffices, options)
+  setInterval(() => {
+      breaker.fire()
+    .then(console.log)
+    .catch(console.error)
+    }, 3000);
+})
 
 client.on('connect', (err) => {
     client.subscribe(deviceRoot + 'dentistoffice');
@@ -45,51 +56,51 @@ client.on('message', (topic, message) => {
 })
 
 const updateDentistOffices = (data) => {
-    try{
-        fetch(url, settings)
-        .then(res => res.json())
-        .then((json) => {
-            
-            var result = [];
-    
-            for(var i = 0; i < json.dentists.length; i++){
-                result.push(json.dentists[i]);
-            }
-    
-            for(var i in result){
-                db.collection('dentistoffices').updateOne(
-                    { "id": result[i].id },
-                    { $set: {
-                        "id": result[i].id,
-                        "name": result[i].name,
-                        "owner": result[i].owner,
-                        "dentists": result[i].dentists,
-                        "address": result[i].address,
-                        "city": result[i].city,
-                        "coordinate": {
-                            "longitude": result[i].coordinate.longitude,
-                            "latitude": result[i].coordinate.latitude
-                        },
-                        "openinghours": {
-                            "monday": result[i].openinghours.monday,
-                            "tuesday": result[i].openinghours.tuesday,
-                            "wednesday": result[i].openinghours.wednesday,
-                            "thursday": result[i].openinghours.thursday,
-                            "friday": result[i].openinghours.friday
-                        }
-                    }},
-                    {upsert: true})
-            }
-        });
-        console.log(' > Dentist office collecton updated.')
+    return new Promise((resolve, reject) => {
 
-
-    } catch(e) {
-         publish('log/error', e);
-         console.log(e);
-    }
-    
-    setTimeout(updateDentistOffices, 600000);
+            fetch(url, settings)
+            .then(res => res.json())
+            .then((json) => {
+                
+                var result = [];
+        
+                for(var i = 0; i < json.dentists.length; i++){
+                    result.push(json.dentists[i]);
+                }
+        
+                for(var i in result){
+                    db.collection('dentistoffices').updateOne(
+                        { "id": result[i].id },
+                        { $set: {
+                            "id": result[i].id,
+                            "name": result[i].name,
+                            "owner": result[i].owner,
+                            "dentists": result[i].dentists,
+                            "address": result[i].address,
+                            "city": result[i].city,
+                            "coordinate": {
+                                "longitude": result[i].coordinate.longitude,
+                                "latitude": result[i].coordinate.latitude
+                            },
+                            "openinghours": {
+                                "monday": result[i].openinghours.monday,
+                                "tuesday": result[i].openinghours.tuesday,
+                                "wednesday": result[i].openinghours.wednesday,
+                                "thursday": result[i].openinghours.thursday,
+                                "friday": result[i].openinghours.friday
+                            }
+                        }},
+                        {upsert: true})
+                }
+                console.log(' > Dentist office collecton updated.')
+                resolve({data: "Success"})
+            }).catch(err => {
+                publish('log/error', err);
+                console.log(err);
+                reject({data: "Failure"})
+            });
+        
+    })
 }
 
 const getAllDentistOffices = () => {
