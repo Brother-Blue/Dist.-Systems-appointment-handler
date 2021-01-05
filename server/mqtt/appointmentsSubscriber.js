0087e700a31ab388217c61a177d50a459f365d26
linusab <linusab@student.chalmers.se>
2021-01-05 12:14:16 +0100

    Added comments to sppointmentsSubscriber

--- server/mqtt/appointmentsSubscriber.js
+++ server/mqtt/appointmentsSubscriber.js
@@ -11,26 +11,30 @@ dotenv.config();
 const options = {
   timeout: 10000, //If function takes longer than 10 sec, trigger a failure
   errorHandlingPercentage: 10, //If 10% of requests fail, trigger circuit
-  resetTimeout: 10000, //After 30 seconds try again
+  resetTimeout: 10000, //After 10 seconds try again
 };
 
 let db;
 
+//connect to the mosquitto broker
 const client = mqtt.connect({
   host: process.env.MQTT_HOST,
   port: process.env.MQTT_PORT,
 });
 
+//When client goes offline, unsubscribe
 client.on("offline", () => {
   console.log("Offline");
   client.unsubscribe(root + "appointments");
 });
 
+//When the client is closed we unsubscribe
 client.on("close", () => {
   console.log("Close");
   client.unsubscribe(root + "appointments");
 });
 
+//Connection to the Mongo cluster
 mongoClient.connect(
   "mongodb+srv://123123123:123123123@cluster0.5paxo.mongodb.net/Cluster0?retryWrites=true&w=majority",
   { useUnifiedTopology: true },
@@ -40,18 +44,24 @@ mongoClient.connect(
   }
 );
 
+//When the client is connected we subscribe
 client.on("connect", (err) => {
   client.subscribe(root + "appointments");
 });
 
 let breakerstate;
 let breaker;
-// On message, run method depending on message
+
+// On message, run method depending on provided message in payload
 client.on("message", (topic, message) => {
   let data = JSON.parse(message);
   
   switch (data.method) {
     case "add":
+      /*Creates a circuit-breaker that wraps the function in order to track the load of the component
+      when the load is too high we open the circuit, after the time reset we enter a half-open state.
+      if the subsequent message is successfull the circuit is closed. The fallback handles the cases where
+      there is to much load on the component. This logic applies to all functions in this switch case */
       breaker = new CircuitBreaker(insertAppointment(data), options);
       breaker.fallback(() => "Sorry, out of service right now");
       breaker.on("open", () => { 
@@ -167,21 +177,25 @@ client.on("message", (topic, message) => {
   }
 });
 
-// Store new appointment on publish message
+// Function to check for duplicate appointments and inserting to database
 const insertAppointment = (data) => {
   return new Promise((resolve, reject) => {
+    // Make request to database to get all booked appointments at a specific time at a specific office
     db.collection("appointments")
       .find({ time: data.time, dentistid: data.dentistid })
       .toArray()
       .then((appointmentresult) => {
+        // Make request to the database to get data about the specific dentist office
         db.collection("dentistoffices")
           .find({ id: parseInt(data.dentistid) })
           .toArray()
           .then((officeresult) => {
+            // Check if there are no appointments for the specified time or the amount of dentist is larger than the appointments booked.
             if (
               appointmentresult == "" ||
               appointmentresult.length < parseInt(officeresult[0].dentists)
             ) {
+              // Insert the new appointment to the database
               db.collection("appointments")
                 .insertOne({
                   userid: data.userid,
@@ -191,6 +205,7 @@ const insertAppointment = (data) => {
                   time: data.time,
                 })
                 .then(() => {
+                  // If the insertion was successfull, filter the data and publish to broker.
                   let payload = JSON.stringify({
                     date: data.time,
                     emailaddress: data.emailaddress,
@@ -208,6 +223,7 @@ const insertAppointment = (data) => {
                     success: true,
                   });
                   let date = data.time.split(" ")[0];
+                  // Send a request to update available time-slots.
                   publish(
                     "dentistoffice",
                     JSON.stringify({
@@ -217,7 +233,7 @@ const insertAppointment = (data) => {
                     }),
                     2
                   );
-
+                  // Publish successfull response to the broker.
                   publish("appointments/response", response, 1);
                   resolve({ data: "Success" });
                 })
@@ -228,7 +244,7 @@ const insertAppointment = (data) => {
                     time: data.time,
                     success: false,
                   });
-
+                  // if there is an error, publish failures to broker. 
                   publish("log/error", err, 2);
                   publish("appointments/response", response, 1);
                   reject({ data: "Failure" });
@@ -240,7 +256,7 @@ const insertAppointment = (data) => {
                 time: data.time,
                 success: false,
               });
-
+              // if there is an error, publish failures to broker.
               publish("log/error", "Appointment insertion failed", 2);
               publish("appointments/response", response, 1);
               reject({ data: "Failure" });
